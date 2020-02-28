bd7d631513a0c480082384a5e078ad549d67c480
Andrii Shevchenko <Andrii.Shevchenko@nice.com>
2020-02-28 14:15:01 +0200

    no message

---
+++ ProductSynchronizer/Entities/ShoeContext.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductSynchronizer.Entities
+{
+    public class ShoeContext : ISizeMapNode
+    {
+        public int Quantity { get; set; }
+        public string InternalSize { get; set; }
+        public string ExternalSize { get; set; }
+        public string Price { get; set; }
+    }
+}
